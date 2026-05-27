eb23923255614e806f2d5db5c926ef92be9b248f
dxjkk <80508494+dxjkk@users.noreply.github.com>
2026-05-27 11:46:15 +0800

    style: restyle theme to WWDC bento liquid glass aesthetic

--- footer.php
+++ footer.php
@@ -61,6 +61,15 @@
         </div>
     </div>
 
+
+    <svg style="display:none" aria-hidden="true">
+        <filter id="glass-distortion">
+            <feTurbulence type="fractalNoise" baseFrequency="0.01 0.01" numOctaves="1" seed="5"/>
+            <feGaussianBlur stdDeviation="3" result="softMap"/>
+            <feDisplacementMap in="SourceGraphic" in2="softMap" scale="150" xChannelSelector="R" yChannelSelector="G"/>
+        </filter>
+    </svg>
+
     <?php $this->footer(); ?>
 
     <script>
